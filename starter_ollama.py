3d886d4aecdbbe76c84c4efdf21680459f94973f
Jamison Proctor <jamison.proctor@gmail.com>
2024-05-21 09:33:59 +0200

    ollama template created

--- starter_ollama.py
+++ starter_ollama.py
@@ -1,5 +1,7 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
-from dotenv import load_dotenv
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.llms.ollama import Ollama
+
 import os, os.path
 
 import logging
@@ -8,11 +10,15 @@ import sys
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# nomic embedding model
+Settings.embed_model = OllamaEmbedding(model_name="nomic-embed-text")
+
+# ollama
+Settings.llm = Ollama(model="llama3", request_timeout=360.0)
+
 
 # check if storage already exists
-PERSIST_DIR = "./openai_storage"
+PERSIST_DIR = "./ollama_storage"
 if not os.path.exists(PERSIST_DIR):
     # load the documents and create the index
     documents = SimpleDirectoryReader("data").load_data()
@@ -24,6 +30,12 @@ else:
     storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
     index = load_index_from_storage(storage_context)
 
+
+
+index = VectorStoreIndex.from_documents(
+    documents,
+)
+
 query_engine = index.as_query_engine()
-response = query_engine.query("What are dinosaurs?")
+response = query_engine.query("Tell me more about Dinosaurs. I want to know everything.")
 print(response)
